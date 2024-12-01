3873e8b8b593c44fd34bd52630f751f4bcd7faf8
Adam Pavlíček <adam.cb@seznam.cz>
2024-12-01 09:12:24 +0100

    01 completed

--- 01/a.cpp
+++ 01/a.cpp
@@ -3,7 +3,24 @@
 using namespace std;
 
 int main(){
+    vector<int> l;
+    vector<int> r;
+
+    int a, b;
+    while(cin >> a >> b){
+        l.push_back(a);
+        r.push_back(b);
+    }
+
+    sort(l.begin(), l.end());
+    sort(r.begin(), r.end());
+
+    int sum = 0;
+    for (size_t i = 0; i < l.size(); i++){
+        sum += abs(l[i] - r[i]);
+    }
     
+    cout << sum << endl;
 
     return 0;
 }
--- 01/b.cpp
+++ 01/b.cpp
@@ -3,7 +3,25 @@
 using namespace std;
 
 int main(){
-    
+    vector<int> l;
+    vector<int> r;
+    map<int, int> occur;
+
+    int a, b;
+    while(cin >> a >> b){
+        l.push_back(a);
+        if(occur.find(b) != occur.end())
+            occur[b]++;
+        else occur.insert({b, 1});
+    }
+
+    long long sum = 0;
+    for(int x : l){
+        if(occur.find(x) != occur.end())
+            sum += (long long)x * occur[x];
+    }
+
+    cout << sum << endl;
 
     return 0;
 }
